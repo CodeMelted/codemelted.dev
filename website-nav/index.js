1c5afd0406da1c580a5605b932a46a459d656ae6
Mark Shaffer <31354065+CodeMelted@users.noreply.github.com>
2021-03-19 22:01:33 -0400

    Fixes to main page header

--- website-nav/index.js
+++ website-nav/index.js
@@ -72,6 +72,7 @@ setTimeout(() => {
                 </div>
             </div>
         <hr style="margin: 0;" /> 
+        <div style="text-align: center"> <br /> <a href="https://www.buymeacoffee.com/CodeMelted"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=CodeMelted&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff"></a></div>
         `;
 
     // Go apply the template
